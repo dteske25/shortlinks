b9717be49c0504be0734c3f3114618a712700a26
Daric Teske <tesked@outlook.com>
2024-12-29 22:33:17 -0600

    clean up functions

--- functions/api/get/[id].ts
+++ functions/api/get/[id].ts
@@ -2,18 +2,12 @@ import { IShortenedLink } from "../../../types";
 
 export const onRequest = async (context) => {
   const id = context.params.id;
-  // const results = await context.env.SHORTLINKS.list();
-  // const ids = results.keys.map((k) => k.name);
-  // const shortlinks: IShortenedLink[] = [];
-  // for (const id of ids) {
-  //   const link = await context.env.SHORTLINKS.get(id);
-  //   if (link) {
-  //     shortlinks.push({
-  //       id,
-  //       ...JSON.parse(link),
-  //     });
-  //   }
-  // }
-  // return new Response(JSON.stringify(shortlinks));
-  return new Response(id);
+  const data = await context.env.SHORTLINKS.get(id);
+
+  const link = JSON.parse(data);
+  link.clicks += 1;
+
+  await context.env.SHORTLINKS.put(id, JSON.stringify(link));
+
+  return new Response(JSON.stringify({ id, ...link }));
 };
